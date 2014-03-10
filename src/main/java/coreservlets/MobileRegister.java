428e53bb352254da8b8f5e1dd5123451e3caa0d1
Seph Thompson <sephthompson@gmail.com>
2014-03-10 13:40:32 -0400

    MobileRegister

--- src/main/java/coreservlets/MobileRegister.java
+++ src/main/java/coreservlets/MobileRegister.java
@@ -12,27 +12,35 @@ import javax.servlet.http.HttpSession;
 
 //import javax.servlet.http.HttpSession;
 
-public class MobileLogin extends HttpServlet {
+public class MobileRegister extends HttpServlet {
 
 	private static final long serialVersionUID = 1029849297015934427L;
-	// private static String pattern1 = "[\\s\\,]";
-	// private static String pattern2 = "[\\s]";
+	private static String pattern1 = "[\\s\\,]";
+	private static String pattern2 = "[\\s]";
 
 	protected void processRequest(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html;charset=UTF-8");
 		
-		String email = request.getParameter("email");
-		String password = request.getParameter("password");
-		String searchQuery = "SELECT * FROM accounts, profiles"
-				+ " WHERE email = '" + email + "'" + " AND password = '"
-				+ password + "'" + " AND accounts.user_id = profiles.user_id;";
+		String user_id = request.getParameter("user_id");
+		String email = request.getParameter("email").replaceAll(pattern1, "");
+		String password = request.getParameter("password").replaceAll(pattern2,
+				"");
+		String firstname = request.getParameter("firstname");
+		String lastname = request.getParameter("lastname");
+
+		String preppedQuery = "INSERT INTO accounts (email, password)"
+				+ " VALUES (?, ?);" + "INSERT INTO profiles (user_id)"
+				+ " SELECT accounts.user_id" + " FROM accounts"
+				+ " WHERE accounts.email = ?;"
+				+ "INSERT INTO profiles (firstname, lastname)"
+				+ " VALUES (?, ?);";
 		
 		Connection con = null;
-		Statement stmt = null;
-		// PreparedStatement ps = null;
+		// Statement stmt = null;
+		PreparedStatement ps = null;
 		ResultSet rs = null;
-		
+
 		try {
 			Class.forName("org.postgresql.Driver");
 		} catch (ClassNotFoundException e) {
@@ -41,35 +49,36 @@ public class MobileLogin extends HttpServlet {
 		try {
 			String url = "jdbc:postgresql://ec2-23-23-81-171.compute-1.amazonaws.com:5432/d3der2cpdnsd7k?user=oougodzmcwhapf&password=srdrgT5PV-VxBxlDGBPtzmFfsg";
 			con = DriverManager.getConnection(url);
-			stmt = con.createStatement();
-			rs = stmt.executeQuery(searchQuery);
-			boolean isEmpty = rs.next();
+
+			System.out.println(preppedQuery); // DEBUG
+
+			ps = con.prepareStatement(preppedQuery);
 			
 			PrintWriter rw = response.getWriter(); // WRITER
+
+			ps.setString(1, email);
+			ps.setString(2, password);
+			ps.setString(3, email);
+			ps.setString(4, firstname);
+			ps.setString(5, lastname);
+
+			System.out.println("SQL=" + preppedQuery.toString()); // DEBUG
+
+			ps.execute();
+
+			System.out.println("Executed=" + preppedQuery.toString()); // DEBUG
+
+			HttpSession session = request.getSession(true);
+			session.setAttribute("user_id", user_id);
+			session.setAttribute("email", email);
+			session.setAttribute("password", password);
+			session.setAttribute("firstname", email);
+			session.setAttribute("lastname", password);
 			
-			if (!isEmpty) {
-				// Redirect to error page
-				// response.sendRedirect("LoginFailure.jsp");
-				
-				rw.print("failure");
+			// RESPONSE TO CLIENT
+			// response.sendRedirect("welcome.jsp");
+			rw.write("success");
 
-			} else if (isEmpty) {
-				/*
-				 * Fetch the session from request, create new session if session
-				 * is not present in the request.
-				 */
-				HttpSession session = request.getSession(true);
-				session.setAttribute("email", rs.getString("email"));
-				session.setAttribute("firstname", rs.getString("firstname"));
-				session.setAttribute("lastname", rs.getString("lastname"));
-				session.setAttribute("about", rs.getString("about"));
-
-				// Redirect to success page
-				// response.sendRedirect("LoginSuccess.jsp");
-				
-				rw.print("success");
-
-			}
 		} catch (SQLException e) {
 			System.out.println("SQLException occured: " + e.getMessage());
 			e.printStackTrace();
@@ -79,6 +88,11 @@ public class MobileLogin extends HttpServlet {
 		            rs.close();
 		        } catch (SQLException e) { /* ignored */}
 		    }
+		    if (ps != null) {
+		        try {
+		            ps.close();
+		        } catch (SQLException e) { /* ignored */}
+		    }
 		    if (con != null) {
 		        try {
 		            con.close();
@@ -86,7 +100,7 @@ public class MobileLogin extends HttpServlet {
 		    }
 		}
 	}
-
+	
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		processRequest(request, response);
