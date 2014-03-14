c9a763d5b9e483485dfb2c86b38e31b53f8a0048
Seph Thompson <sephthompson@gmail.com>
2014-03-14 00:45:54 -0400

    dfgdfg

--- src/main/java/coreservlets/MobileProfile.java
+++ src/main/java/coreservlets/MobileProfile.java
@@ -1,9 +1,12 @@
 package coreservlets;
 
+import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.Writer;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
@@ -12,24 +15,25 @@ public class MobileProfile extends HttpServlet {
 
 	private static final long serialVersionUID = 5612023750818929781L;
 
-	private Writer output;
-	
-	PrintWriter pw = new PrintWriter(output);
-
-	public void doGet() throws JSONException {
-
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		
+		PrintWriter rw = response.getWriter(); // WRITER
+		
 		JSONObject json = new JSONObject();
-		json.put("email", "japple@email.com");
-		json.put("firstname", "Johnny");
-		json.put("lastname", "Appleseed");
-		json.put("about", "This user currently has no information.");
 		
 		try {
-			pw.write(json.toString());
-		} catch (Exception e) {
+			json.put("email", "japple@email.com");
+			json.put("firstname", "Johnny");
+			json.put("lastname", "Appleseed");
+			json.put("about", "This user currently has no information.");
+		} catch (JSONException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			rw.print(e);
 		}
+		
+		rw.print(json.toString());
 	}
 
 }
