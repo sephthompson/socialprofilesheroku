5ba97829221768507fa5004b828c7f91a1e457f8
Seph Thompson <sephthompson@gmail.com>
2014-03-16 01:49:52 -0400

    fgdfgdfdgasfgdsa

--- src/main/java/coreservlets/MobileProfile.java
+++ src/main/java/coreservlets/MobileProfile.java
@@ -16,7 +16,7 @@ public class MobileProfile extends HttpServlet {
 
 	private static final long serialVersionUID = 5612023750818929781L;
 
-	protected void doPost(HttpServletRequest request,
+	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html;charset=UTF-8");
 		
