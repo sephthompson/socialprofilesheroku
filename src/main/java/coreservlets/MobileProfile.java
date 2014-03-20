f9961afa4b9569cc1929e81c71ca0da01ac361d3
Seph Thompson <sephthompson@gmail.com>
2014-03-20 05:03:44 -0400

    ertdfgrdggr

--- src/main/java/coreservlets/MobileProfile.java
+++ src/main/java/coreservlets/MobileProfile.java
@@ -18,7 +18,7 @@ public class MobileProfile extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html;charset=UTF-8");
+		response.setContentType("application/json;charset=UTF-8");
 		
 		PrintWriter rw = response.getWriter(); // WRITER
 		
